99b8694f4339df0f35cd444ad18bd8cf6b14d144
Yuren Ju <yurenju@gmail.com>
2020-03-17 19:23:57 +0800

    support typescript

--- test/ske.test.ts
+++ test/ske.test.ts
@@ -8,13 +8,13 @@ describe("Counter smart contract", () => {
   const provider = new MockProvider();
   const [wallet] = provider.getWallets();
 
-  async function deployToken(initialValue) {
+  async function deployToken(initialValue: string) {
     const token = await deployContract(wallet, Token, [initialValue]);
     return token;
   }
 
   it("sets initial balance in the constructor", async () => {
-    const token = await deployToken(10000);
+    const token = await deployToken("10000");
     expect(await token.balanceOf(wallet.address)).to.equal("10000");
   });
 });
