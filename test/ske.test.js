79ad8f02a872fa2ddabad5ad9b85487755fab147
Yuren Ju <yurenju@gmail.com>
2020-03-17 18:51:20 +0800

    added test

---
+++ test/ske.test.js
@@ -0,0 +1,20 @@
+const { use, expect } = require("chai");
+const { solidity, MockProvider, deployContract } = require("ethereum-waffle");
+const Token = require("../build/SKEToken.json");
+
+use(solidity);
+
+describe("Counter smart contract", () => {
+  const provider = new MockProvider();
+  const [wallet] = provider.getWallets();
+
+  async function deployToken(initialValue) {
+    const token = await deployContract(wallet, Token, [initialValue]);
+    return token;
+  }
+
+  it("sets initial balance in the constructor", async () => {
+    const token = await deployToken(10000);
+    expect(await token.balanceOf(wallet.address)).to.equal("10000");
+  });
+});
